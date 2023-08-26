4d9cda077d2a43e256ccacbabb322020b8a14148
Hassan Ahmed <hassanah.dev@gmail.com>
2023-08-27 01:08:00 +0500

    Added Navbar and image

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template(template_name_or_list='index.html', active='home')
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template(template_name_or_list='about.html', title="About", active='about')
 
 
 if __name__ == "__main__":
